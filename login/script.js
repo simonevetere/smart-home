3349c3a63f0b9c49a5623d86b17f065a1de9b6ee
Simone Vetere <simoneveterex@gmail.com>
2024-10-21 18:15:26 +0200

    fix

--- login/script.js
+++ login/script.js
@@ -1,3 +1,15 @@
+function getCookie(name) {
+  let cookieArr = document.cookie.split(";");
+  for(let i = 0; i < cookieArr.length; i++) {
+    let cookiePair = cookieArr[i].split("=");
+    if(name == cookiePair[0].trim()) {
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+  return null;
+}
+
+
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Previene l'invio del form
 
